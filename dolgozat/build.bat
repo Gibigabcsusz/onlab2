59c768c8941ef6349762d1800016683d0403cf0b
Gibigabcsusz <gbr.szilagyi@gmail.com>
2022-11-15 17:57:52 +0100

    Bevezetés átdolgozása

--- dolgozat/build.bat
+++ dolgozat/build.bat
@@ -2,16 +2,16 @@ ECHO OFF
 set cim="onlab2"
 echo "Forditas pdflatex 1"
 echo "Forditas pdflatex 1\n\n\n" > output.txt
-pdflatex -interaction=nonstopmode -halt-on-error %cim%.tex >> output.txt
+pdflatex -interaction=nonstopmode -halt-on-error %cim%.tex > output.txt
 echo "Forditas bibtex"
-echo "\n\n\nForditas bibtex\n\n\n" >> output.txt
-bibtex %cim% >> output.txt
+echo "\n\n\nForditas bibtex\n\n\n" > output.txt
+bibtex %cim% > output.txt
 echo "Forditas pdflatex 2"
-echo "\n\n\nForditas pdflatex 2\n\n\n" >> output.txt
-pdflatex -interaction=nonstopmode -halt-on-error %cim%.tex >> output.txt
+echo "\n\n\nForditas pdflatex 2\n\n\n" > output.txt
+pdflatex -interaction=nonstopmode -halt-on-error %cim%.tex > output.txt
 echo "Forditas pdflatex 3"
-echo "\n\n\nForditas pdflatex 3\n\n\n" >> output.txt
-pdflatex -interaction=nonstopmode -halt-on-error %cim%.tex >> output.txt
+echo "\n\n\nForditas pdflatex 3\n\n\n" > output.txt
+pdflatex -interaction=nonstopmode -halt-on-error %cim%.tex > output.txt
 
 if exist %cim%.aux rm %cim%.aux
 if exist %cim%.bbl rm %cim%.bbl
